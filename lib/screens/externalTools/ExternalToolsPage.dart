ddc1d6b9bf708a6b11f63d64478ff9c2e37fc3e2
Giovanni Barbaro <vanni-96@hotmail.it>
2022-12-21 21:35:49 +0100

    Improves External Tools screen

--- lib/screens/externalTools/ExternalToolsPage.dart
+++ lib/screens/externalTools/ExternalToolsPage.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_apps/static/externalTools/externalTools.dart';
+import 'package:flutter_apps/static/externalTools/external_tools.dart';
 import 'package:flutter_apps/widgets/LunatechScaffold.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
@@ -17,9 +17,9 @@ class ExternalToolsPage extends StatelessWidget {
 
   Widget body(BuildContext context) {
     return GridView.count(
-      crossAxisCount: 2,
+      crossAxisCount: 3,
       crossAxisSpacing: 8,
-      mainAxisSpacing: 8,
+      childAspectRatio: 0.7,
       children: List.generate(externalTools.length,
           (index) => buildExternalTool(externalTools[index], context)),
     );
@@ -31,15 +31,27 @@ class ExternalToolsPage extends StatelessWidget {
       child: GestureDetector(
         onTap: () => launchUrlString(
             Platform.isAndroid ? externalTool.androidUrl : externalTool.iosUrl),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FractionallySizedBox(
-                widthFactor: 0.65, child: Image.network(externalTool.image)),
-            Padding(
-                padding: const EdgeInsets.all(5),
-                child: Text(externalTool.name))
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              FractionallySizedBox(
+                  widthFactor: 0.65,
+                  child: ClipRRect(
+                      borderRadius: const BorderRadius.all(Radius.circular(20)),
+                      child: Image.network(externalTool.image))),
+              Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Text(externalTool.name)),
+              Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Text(externalTool.description ?? "",
+                      textAlign: TextAlign.center,
+
+                      style: TextStyle(color: Theme.of(context).textTheme.bodyMedium?.color?.withOpacity(0.70))))
+            ],
+          ),
         ),
       ),
     );
--- lib/static/externalTools/external_tools.dart
+++ lib/static/externalTools/external_tools.dart
@@ -3,12 +3,14 @@ class ExternalToolInfo {
       {required this.name,
       required this.androidUrl,
       required this.iosUrl,
-      required this.image});
+      required this.image,
+      this.description});
 
   String name;
   String androidUrl;
   String iosUrl;
   String image;
+  String? description;
 }
 
 final List<ExternalToolInfo> externalTools = [
@@ -27,7 +29,8 @@ final ExternalToolInfo _toggle = ExternalToolInfo(
     iosUrl:
         "https://apps.apple.com/us/app/expensify-receipts-expenses/id471713959",
     image:
-        "https://play-lh.googleusercontent.com/PeblLXajnpQMBdnzHCQ9yRh6IZ1iOM7qqJkp306uOYlYq8djKFs2vTLO5YO265MPmcM=w240-h480-rw");
+        "https://play-lh.googleusercontent.com/PeblLXajnpQMBdnzHCQ9yRh6IZ1iOM7qqJkp306uOYlYq8djKFs2vTLO5YO265MPmcM=w240-h480-rw",
+    description: "Keep track of your work");
 
 final ExternalToolInfo _expensify = ExternalToolInfo(
     name: "Expensify",
@@ -36,14 +39,16 @@ final ExternalToolInfo _expensify = ExternalToolInfo(
     iosUrl:
         "https://apps.apple.com/us/app/toggl-track-hours-time-log/id1291898086?platform=iphone",
     image:
-        "https://play-lh.googleusercontent.com/s-RdhKt0Puppli-DZgjT0dLsxwYr3AqFc6VWmgXM5641mkFyohapOkExeVJXxh_vIA=w480-h960-rw");
+        "https://play-lh.googleusercontent.com/s-RdhKt0Puppli-DZgjT0dLsxwYr3AqFc6VWmgXM5641mkFyohapOkExeVJXxh_vIA=w480-h960-rw",
+    description: "Get refunds for your work related purchases");
 
 final ExternalToolInfo _slack = ExternalToolInfo(
     name: "Slack",
     androidUrl: "https://play.google.com/store/apps/details?id=com.Slack",
     iosUrl: "https://apps.apple.com/us/app/slack/id618783545",
     image:
-        "https://downloadr2.apkmirror.com/wp-content/uploads/2021/05/65/609315d878257.png");
+        "https://downloadr2.apkmirror.com/wp-content/uploads/2021/05/65/609315d878257.png",
+    description: "Chat with your colleagues!");
 
 final ExternalToolInfo _confluence = ExternalToolInfo(
     name: "Confluence",
@@ -51,7 +56,8 @@ final ExternalToolInfo _confluence = ExternalToolInfo(
         "https://play.google.com/store/apps/details?id=com.atlassian.android.confluence.core",
     iosUrl: "https://apps.apple.com/us/app/confluence-cloud/id1006971684",
     image:
-        "https://is2-ssl.mzstatic.com/image/thumb/Purple112/v4/1c/db/ee/1cdbeee7-07b7-4740-21d5-a1a2d628d8d0/AppIcon-1x_U007emarketing-0-10-0-85-220.png/246x0w.webp");
+        "https://is2-ssl.mzstatic.com/image/thumb/Purple112/v4/1c/db/ee/1cdbeee7-07b7-4740-21d5-a1a2d628d8d0/AppIcon-1x_U007emarketing-0-10-0-85-220.png/246x0w.webp",
+    description: "Documentation on the go");
 
 final ExternalToolInfo _gmail = ExternalToolInfo(
     name: "Gmail",
@@ -59,4 +65,5 @@ final ExternalToolInfo _gmail = ExternalToolInfo(
         "https://play.google.com/store/apps/details?id=com.google.android.gm",
     iosUrl: "https://apps.apple.com/us/app/gmail-email-by-google/id422689480",
     image:
-        "https://play-lh.googleusercontent.com/KSuaRLiI_FlDP8cM4MzJ23ml3og5Hxb9AapaGTMZ2GgR103mvJ3AAnoOFz1yheeQBBI=w240-h480-rw");
+        "https://play-lh.googleusercontent.com/KSuaRLiI_FlDP8cM4MzJ23ml3og5Hxb9AapaGTMZ2GgR103mvJ3AAnoOFz1yheeQBBI=w240-h480-rw",
+    description: "Emails, and more!");
